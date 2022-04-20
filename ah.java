b6b821abb054d010d1bce8c7694ecf37d436e8c5
kboeh002 <89881818+kboeh002@users.noreply.github.com>
2022-04-20 17:32:58 -0400

    d

---
+++ ah.java
@@ -0,0 +1,54 @@
+class FeatureSet() {
+
+boolean[] bArr = {false};
+
+  public boolean[] getFeatures(String theWord){
+    // check 1
+    bArr[0] = ...
+    // check 2
+    ...
+    // check 3 ...
+    ...
+
+    return bArr;
+  }
+
+}
+
+
+class Token() {
+  String theWord = "";
+  boolean[] arr = {false};
+  setFuckingFeatures(String theWord);
+
+  setFuckingFeatures(String theWord){
+      arr = feature.getFeatures(theWord); // will return a bool array
+  }
+
+}
+
+FeatureSet[] feature;
+
+class Block(){
+private ArrayList<Token> tokens;
+  
+this.rawBlock = block.split("\b");     /** splitting raw block by everything */
+        this.tokens = new ArrayList<>(rawBlock.length);
+  
+  
+  for(String rb : this.rawBlock){
+            this.tokens.add(new Token(rb));
+        }
+ 
+  /** 
+  * after tokens created we will have a list of tokens with only the word, our bool array will be empty
+  * this is why we are putting it in the for loop 
+  * so now we are going through each token and setting the features
+  */
+  
+  for(Token t: this.tokens){
+  /** t contains our word... */
+      t.setFeatures();
+  }
+  
+}
